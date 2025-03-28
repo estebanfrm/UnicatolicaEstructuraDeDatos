1183ba5d3eb90477dcae5ab043264b2c29b89e44
estebanfrm <giraldomontenegro2304@gmail.com>
2025-03-28 00:19:04 -0500

    Update Pilas lista enlazada.py

--- CORTE 2/Pilas lista enlazada.py
+++ CORTE 2/Pilas lista enlazada.py
@@ -1,27 +1,27 @@
-class Node:
+class Node: 
     def __init__(self, value):
-        self.value = value
-        self.next = None
+        self.value = value #Almacena el valor del nodo
+        self.next = None #apunta al siguiente nodo en la pila, inicialmente es None
 
 class Stack:
     def __init__(self):
-        self.head = None  # Inicializa la pila con head como None
+        self.head = None  # Inicializa la pila vacia, sin nodos
 
     def push(self, value):
-        node = Node(value)  # Crea un nuevo nodo con el valor
+        node = Node(value)  # Crea un nuevo nodo con el valor dado
         node.next = self.head  # Apunta el nuevo nodo al anterior head
         self.head = node  # Actualiza head al nuevo nodo
 
     def pop(self):
-        value = None
-        if self.head is not None:
-            value = self.head.value
-            self.head = self.head.next
-        return value
+        value = None #se inicializa en None
+        if self.head is not None: #Verifica si la pila no esta vacia
+            value = self.head.value #guarda el valor del nodo en la cima
+            self.head = self.head.next #Mueve la cabeza al siguiente nodo
+        return value #Devuelve el valor eliminado
 
     #Ejemplo de uso:
 
-stack = Stack()  #Crea pila con tamaño inicial de 5
+stack = Stack()  #Crea la pila
 stack.push(10)
 stack.push(20)
 stack.push(30)
